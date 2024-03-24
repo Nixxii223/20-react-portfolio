4ca2acb7ddb01401931e85d024012303682be1d3
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-24 01:37:56 -0400

    footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import './styles.css';   
+import { Box, Link, IconButton, Container } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import './styles.css';
 
-const Footer = () => {
+
+export default function Footer() {
     return (
-        <footer className='poppins-regular'>
-            <p>Made with ❤️ by Nicki Barrett 2024</p>
-        </footer>
+        <Container className="footer-container">
+            <Box sx={{ display: 'flex', justifyContent: 'center', padding: '0', margin: '0' }}>
+                
+                <Link href="https://github.com/nixxii223" target="_blank" rel="noopener noreferrer">
+                    <IconButton>
+                        <GitHubIcon />
+                    </IconButton>
+                </Link>
+                <Link href="www.linkedin.com/in/nicki-barrett" target="_blank" rel="noopener noreferrer">
+                    <IconButton>
+                        <LinkedInIcon />
+                    </IconButton>
+                </Link>
+            </Box>
+            <p className='poppins-regular'>Made with ❤️ by Nicki Barrett 2024</p>
+        </Container>
     );
 }
-
-export default Footer;
