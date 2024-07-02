00eed2149b6092d61a40120990c01c6b5b4471ef
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-07-02 15:05:47 -0400

    update homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,7 +5,7 @@ export default function Homepage() {
         <div>
             <h2 className="birthstone-bounce-medium">About Me</h2>
             <p className="poppins-regular">
-                Hello, my name is Nicki Barrett and I am currently studying full stack web development through the University of Utah in partnership with edX. I also hold a Master of Science degree in Speech-Language Pathology and have spent the past ten years working as a feeding therapist in the Newborn ICU. My goal is to merge my love of feeding therapy with my new-found love of technology to create applications that work for therapists, rather than against them.
+                Hello, my name is Nicki Barrett and I recently completed a certification in full stack web development through the University of Utah in partnership with edX. I also hold a Master of Science degree in Speech-Language Pathology and have spent the past ten years working as a feeding therapist in the Newborn ICU. My goal is to merge my love of feeding therapy with my new-found love of technology to create applications and web-based learning programs that work for therapists, rather than against them.
 
                 Check out my portfolio with projects showcasing my abilities in full stack coding!
             </p>
